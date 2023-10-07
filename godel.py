1ccb053c08c918fe7c044a47950f625f2e4125a1
Patautista <caleb.kart@gmail.com>
2023-10-06 23:57:51 -0300

    added encoder

--- godel.py
+++ godel.py
@@ -12,7 +12,7 @@ def inverse_pair(result):
         y = 0
     return x, y
 
-prime_factors = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]  # Prime numbers for encoding
+prime_factors = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
 
 def encode_godel_number(lst):
     if not lst:
@@ -41,33 +41,92 @@ def decode_godel_number(number):
     
     return lst
 
-def gerar_linha_de_codigo(a, b, c):
-    # Primeiro, vamos tratar a parte do rótulo
-    rotulo = ""
+def decode_line(a, b, c):
+    label = ""
     if a > 0:
-        letras = "ABCDE"
-        numero_rotulo = (a - 1) // 5 + 1
-        if(numero_rotulo == 1):
-           numero_rotulo = "" 
-        letra_rotulo = letras[(a - 1) % 5]
-        rotulo = f"[{letra_rotulo}{numero_rotulo}]"
+        letters = "ABCDE"
+        label_number = (a - 1) // 5 + 1
+        if(label_number == 1):
+           label_number = "" 
+        label_letter = letters[(a - 1) % 5]
+        label = f"[{label_letter}{label_number}]"
 
-    # Agora, vamos tratar a parte da variável
-    variaveis = ["Y", "X1", "Z1", "X2", "Z2", "X3", "Z3"]  # Lista de variáveis
-    variavel = variaveis[c]
+    variables = ["Y", "X1", "Z1", "X2", "Z2", "X3", "Z3"]
+    variable = variables[c]
 
-    # Em seguida, tratamos o tipo de declaração
     if b == 0:
-        tipo = f"{variavel} ← {variavel}"
+        type = f"{variable} ← {variable}"
     elif b == 1:
-        tipo = f"{variavel} ← {variavel} + 1"
+        type = f"{variable} ← {variable} + 1"
     elif b == 2:
-        tipo = f"{variavel} ← {variavel} - 1"
+        type = f"{variable} ← {variable} - 1"
     else:
-        numero_rotulo = b - 2
-        tipo = f"IF {variavel} != 0 GOTO [{letra_rotulo}{numero_rotulo}]"
+        label_number = b - 2
+        type = f"IF {variable} != 0 GOTO [{label_letter}{label_number}]"
 
-    # Combinamos todas as partes em uma linha de código
-    linha_de_codigo = f"{rotulo} {tipo}"
+    line = f"{label} {type}"
 
-    return linha_de_codigo
+    return line
+
+def encode_line(line):
+    # Split the line into parts (label and type)
+    parts = line.split()
+    label = parts[0]
+    statement = []
+
+    # Initialize variables
+    a = 0
+    b = 0
+    c = 0
+
+    # Decode the label (if present)
+    if label.startswith("[") and label.endswith("]"):
+        label_text = label[1:-1]
+        if len(label_text) == 1:
+            label_letter = label_text
+            label_number = 1
+        else:
+            label_letter = label_text[0]
+            label_number = label_text[1:]
+        letters = "ABCDE"
+        a = (letters.index(label_letter) + 1) + (int(label_number) - 1) * 5
+        statement = parts[1:]
+    else:
+        statement = parts[0:]
+
+    # Decode the type
+    variable = statement[0]
+    operator = statement[1]
+
+    
+    if len(statement) == 5:
+        if operator == "←" and f"{statement[2]} {statement[3]} {statement[4]}" == f"{variable} + 1":
+            b = 1
+        elif operator == "←" and f"{statement[2]} {statement[3]} {statement[4]}" == f"{variable} - 1":
+            b = 2
+    elif operator == "←":
+        b = 0
+    elif operator == "IF":
+        # Extract the label number
+        label_letter = statement[3][1]
+        label_number = statement[3][2:-1]
+        label_index = letters.index(label_letter) + 1
+        b = label_index + int(label_number) * 5 + 2
+
+    # Decode the variable
+    variables = ["Y", "X1", "Z1", "X2", "Z2", "X3", "Z3"]
+    c = 0
+    if(variable[0] == "Y"):
+        c = 0
+    elif(variable[1] == "X"):
+        if(len(variable == 2)):
+            c = int(variable[2]) * 2 - 1
+        else:
+            c = 1
+    elif(variable[1] == "Z"):
+        if(len(variable == 2)):
+            c = int(variable[2]) * 2
+        else:
+            c = 2
+
+    return a, b, c
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import godel
 
-# Exemplo de uso da função:
+
+def universal(args):
+    y = 0
+    n = len(args) - 1
+    z = args[n] + 1
+    s = 1
+    for i in range(n):
+        s ** godel.prime_factors[2 * i] ^ args[i]
+    k = 1
+    if(k == lt(z) + 1 or k == 0):
+        y = s
+    return y
+
 for number in godel.decode_godel_number(200):
     a, r = godel.inverse_pair(number)
     b, c = godel.inverse_pair(r)
-    print(godel.gerar_linha_de_codigo(a, b, c))
+    line = godel.decode_line(a, b, c)
+    print((a,b,c))
+    print("decoded as " + godel.decode_line(a, b, c))
+    print("encoded as " + str(godel.encode_line(line)))
